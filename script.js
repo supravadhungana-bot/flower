299447e33c0d08a8b11cb444198f41929dfa8907
Suprava Dhungana <supravadhungana@gmail.com>
2026-02-14 08:38:46 +0545

    final fixation

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  // Get Nepal time properly
-  const nepalNow = new Date(
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Kathmandu" })
-  );
-
-  const todayNepal = new Date(
-    nepalNow.getFullYear(),
-    nepalNow.getMonth(),
-    nepalNow.getDate()
-  );
-
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-
-    const unlockStr = card.getAttribute("data-unlock");
-    const unlockDate = new Date(unlockStr + "T00:00:00");
-
-    if (todayNepal < unlockDate) {
-
-      card.innerHTML = `
-        <div style="
-          height: 300px;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          text-align: center;
-        ">
-          <h2>🔒 Locked</h2>
-          <p>Opens on ${unlockStr}</p>
-        </div>
-      `;
-    }
-
-  });
-
-});
-
-
-
+ 
 function answerYes() {
   document.getElementById("answerText").innerText = "Yayyy 💗 That made me smile.";
 }
